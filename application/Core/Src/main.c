63dc5c0a278803f85d8918a089ca230b78683d0d
Jayeshraudal <jayeshraudal95@gmail.com>
2026-05-20 17:14:57 +0200

    updated UARTlogic

--- application/Core/Src/main.c
+++ application/Core/Src/main.c
@@ -102,7 +102,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+	  /* HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
 	  HAL_Delay(500);
 
     /* USER CODE BEGIN 3 */
