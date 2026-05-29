3dc5b13507017c83b38b0d07d6ed96eb676da637
Jayeshraudal <jayeshraudal95@gmail.com>
2026-05-29 19:48:30 +0200

    adding FW validation logic

--- Bootloader_dev/Core/Src/bl_jump.c
+++ Bootloader_dev/Core/Src/bl_jump.c
@@ -8,6 +8,7 @@
 #include "main.h"
 #include "bl_jump.h"
 #include "flash_layout.h"
+#include "app_header.h"
 
 
 // function as pointer for appEntry
---
+++ application/Core/Inc/app_header.h
@@ -0,0 +1,25 @@
+/*
+ * 		app_header.h
+ * 		created on 29.05.2026
+ * 		Author: Jayesh Raundal
+ *
+ *
+ * */
+
+#ifndef INC_APP_HEADER_H_
+#define INC_APP_HEADER_H_
+
+#include "flash_layout.h"
+
+typedef struct {
+
+	uint32_t otaflag;
+	uint32_t magic;
+	uint32_t size;   //application size in bytes
+	uint32_t crc;	//crc32 of application
+	uint32_t version;
+
+} app_header_t;
+
+
+#endif
